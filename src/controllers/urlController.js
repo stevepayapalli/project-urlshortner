d4c6ff8d3453ea98ba311cb3bbb30f0385354925
Pritam0044 <you@example.com>
2022-05-18 17:08:05 +0530

    cache added

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -1,7 +1,36 @@
 const urlModel = require("../models/urlModel");
 const nanoId = require("nano-id");
 const validUrl = require("valid-url");
-const baseUrl = "http://localhost:3000";
+const baseUrl = "http://localhost:3000"; 
+
+
+const redis = require("redis");
+const { promisify } = require("util");  /// don't know from where it is coming..??
+
+const redisClient = redis.createClient(
+  10204,
+  "redis-10204.c299.asia-northeast1-1.gce.cloud.redislabs.com",
+  { no_ready_check: true }
+);
+redisClient.auth("XTAwfJw6tbOouRbMmMT7uics1fOnpuYh", function (err) {
+  if (err) throw err;
+});
+
+redisClient.on("connect", async function () {
+  console.log("Connected to Redis..");
+});
+
+
+
+//1. connect to the server
+//2. use the commands :
+
+//Connection setup for redis
+
+const SET_ASYNC = promisify(redisClient.SET).bind(redisClient);
+const GET_ASYNC = promisify(redisClient.GET).bind(redisClient);   /////// END
+
+
 
 const createUrl = async function (req, res) {
   try {
@@ -29,15 +58,29 @@ const createUrl = async function (req, res) {
   }
 };
 
-const getUrl = async function (req, res){
-    const url_code = req.params.urlCode
-    const urlData = await urlModel.findOne({urlCode:url_code})
-    if(!urlData){
-      return  res.status(404).send({status:false, message:"No URL is found with the given code. Please enter valid URL code"})
-        
-    }
-        res.status(302).redirect(urlData.longUrl)
-} 
+const getUrl = async function (req, res) {
+  const getDataFromCache = await GET_ASYNC(`${req.params.urlCode}`);
+  if (getDataFromCache) {
+    // console.log(getDataFromCache)
+    return res.status(302).send(getDataFromCache);
+    
+  } 
+  else {
+    const url_code = req.params.urlCode;
+    const urlData = await urlModel.findOne({ urlCode: url_code }).select({_id:0,longUrl:1});
+    if (!urlData) {
+      return res
+        .status(404)
+        .send({
+          status: false,
+          message:
+            "No URL is found with the given code. Please enter valid URL code",
+        });}
+    await SET_ASYNC(`${req.params.urlCode}`, JSON.stringify(urlData.longUrl))
+    res.status(302).redirect(urlData.longUrl);
+    // console.log(urlData.longUrl)
+  }
+}; 
 
 
 module.exports.createUrl = createUrl;
